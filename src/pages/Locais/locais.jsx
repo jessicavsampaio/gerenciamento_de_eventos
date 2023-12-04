46af2b08cca5f8adf995cf29dafd478a9a45d9f0
jessicavsampaio <8433012+jessicavsampaio@users.noreply.github.com>
2023-12-03 22:10:00 -0300

    Criando as páginas Categorias, Locais e Login

---
+++ src/pages/Locais/locais.jsx
@@ -0,0 +1,18 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+
+export function Locais() {
+    return(
+        <div>
+            <div>
+                <Header />
+            </div>
+            <Section title="Locais" />
+            <div>
+                <Footer />
+            </div>
+        </div>
+
+    )
+}
---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,18 @@
+import { Footer } from "../../components/Footer";
+import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
+
+export function Login() {
+    return(
+        <div>
+            <div>
+                <Header />
+            </div>
+            <Section title="Login" />
+            <div>
+                <Footer />
+            </div>
+        </div>
+
+    )
+}
