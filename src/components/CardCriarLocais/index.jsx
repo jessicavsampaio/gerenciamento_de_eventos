b9e6c975d3196626e186de718f879b1b1d7e6871
Andre Gorgen <andregorgen@gmail.com>
2023-12-08 00:47:37 -0300

    Criando axios dos locais

--- src/components/CardCriarLocais/index.jsx
+++ src/components/CardCriarLocais/index.jsx
@@ -1,21 +1,34 @@
 import { CardCriarLocaisDiv } from './styles'
 import { MdPlace } from "react-icons/md";
+import { useForm } from "react-hook-form"
+import { api } from '../../service/api';
+
+export function CardCriarLocais({ setId }) {
 
 
-export function CardCriarLocais() {
+
+    const { register, handleSubmit } = useForm()
+    const addEvent = data => api.post("http://localhost:3000/place", data)
+        .then(response => {
+            console.log("deu tudo certo.")
+            setId(response.data.id);
+        })
+        .catch(() => {
+            console.log("deu tudo errado.")
+        })
     return (
         <CardCriarLocaisDiv>
             <div className='Container'>
-                <div className='Section'>
+                <form onSubmit={handleSubmit(addEvent)}>
                     <h3>Local</h3>
                     <div className="local">
                         <MdPlace />
                         <p>Local: </p>
                     </div>
-                    <input type="text" name="local" id="local" required />
+                    <input type="text" name="local" id="local" required {...register("name")} />
 
                     <button className="cadastroLocalBtn" type="submit">Cadastrar Local</button>
-                </div>
+                </form>
             </div>
 
 
