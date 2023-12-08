4545ead59990568dad9ac2b24ac40c5ef970dab4
Andre Gorgen <andregorgen@gmail.com>
2023-12-08 00:47:26 -0300

    Criando o axios da categoria

--- src/components/CardCriarCategoria/index.jsx
+++ src/components/CardCriarCategoria/index.jsx
@@ -1,20 +1,29 @@
 import { CardCriarCategoriaDiv } from './styles'
 import { MdCategory } from "react-icons/md";
-
-
+import { useForm } from "react-hook-form"
+import {api} from '../../service/api';
 export function CardCriarCategoria() {
+
+    const { register, handleSubmit } = useForm()
+    const addEvent = data => api.post("http://localhost:3000/category", data)
+        .then(() => {
+            console.log("deu tudo certo.")
+        })
+        .catch(() => {
+            console.log("deu tudo errado.")
+        })
     return (
         <CardCriarCategoriaDiv>
             <div className='Container'>
-                <div className='Section'>
+                <form onSubmit={handleSubmit(addEvent)}>
                     <h3>Categoria</h3>
                     <div className="categoria">
                         <MdCategory />
                         <p>Categoria: </p>
                     </div>
-                    <input type="text" name="categoria" id="categoria" required />
+                    <input type="text" name="categoria" id="categoria" required {...register("name")} />
                     <button className="cadastroCategoriaBtn" type="submit">Cadastrar Categoria</button>
-                </div>
+                </form>
             </div>
         </CardCriarCategoriaDiv>
     )
